a78cf5dfc052df7bfdb0cd1f1e1db4be605025da
Natália de Brites B <113113412+nataliadebrites@users.noreply.github.com>
2024-06-06 11:00:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultados = document.querySelector('.caixa-resultados');
+const textoResultados = document.querySelector('.texto-resultados');
