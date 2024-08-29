01312397814d0e394a704b6cd1481934323dcf14
Natália de Brites B. <113113412+nataliadebrites@users.noreply.github.com>
2024-08-29 11:22:31 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,169 @@
+export const perguntas = [
+    {
+        enunciado: "Você acredita que sua alimentação atual atende aos critérios de uma dieta saudável e equilibrada, ou há áreas que você acha que poderiam ser melhoradas?",
+        alternativas: [
+            {
+                texto: "Sim, possuo uma alimentação saúdavel!",
+                afirmacao: [
+                    "Sua alimentação saudável é a base perfeita para uma vida cheia de energia e bem-estar.",
+                    "Você está investindo no seu futuro com cada escolha alimentar saudável que faz."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Não, minha alimentação não é saúdavel...",
+                afirmacao: [
+                    "Adotar hábitos alimentares mais saudáveis pode transformar sua saúde e bem-estar.",
+                    "Cada passo em direção a uma alimentação equilibrada é um avanço significativo para uma vida mais saudável."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Manter uma alimentação regular é fundamental para a saúde e o bem-estar, tanto mental quanto físico. Estabelecer horários fixos para refeições, como café da manhã, almoço e jantar, pode ser uma excelente estratégia.  Qual dessas duas opçoes seria uma boa escolha para você?",
+        alternativas: [
+            {
+                texto: "Iogurte grego natural com frutas frescas, uma porção de granola e uma colher de chá de sementes de chia.", 
+                afirmacao: [
+                    "Para quem escolheu o iogurte grego com frutas, granola e sementes de chia: você fez uma excelente escolha! Esse café da manhã é nutritivo e fornece uma boa combinação de proteínas, fibras e antioxidantes para começar bem o dia.",
+                    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Donuts e um café com bastante açúcar.",
+                afirmacao: [
+                    "Para quem optou pelos donuts e café com açúcar: essa não foi a melhor escolha. Esse tipo de café da manhã pode levar a picos de açúcar no sangue e falta de energia, e pode não fornecer os nutrientes necessários para um bom início de dia.",
+                    
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Quais são alguns hábitos simples que você pode adotar para tornar sua alimentação mais saudável no dia a dia?",
+        alternativas: [
+            {
+                texto: "Beber água regularmente ao longo do dia.",
+                afirmacao: [
+                    "A hidratação diária promove energia e bem-estar.",
+                    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Beber bebidas açucaradas, como refrigerantes e sucos industrializados, em vez de água.",
+                afirmacao: [
+                    "Aumentar a ingestão de água é essencial para melhorar a saúde e a vitalidade.",
+                    
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Exercícios físicos são essenciais para manter a saúde, fortalecendo o coração, os músculos e melhorando a flexibilidade. Além disso, ajudam a reduzir o estresse e melhorar o humor. Com tantos benefícios, a atividade física é crucial para uma vida saudável. E você, pratica exercícios físicos regularmente?",
+        alternativas: [
+            {
+                texto: "Sim, pratico exercícios físicos regularmente. Sinto que isso realmente melhora minha energia, saúde e bem-estar geral.",
+                afirmacao: [
+                    "Manter uma rotina regular de exercícios físicos contribui significativamente para a saúde e o bem-estar, trazendo energia e melhorando a qualidade de vida.",
+                    
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Não, atualmente não pratico exercícios físicos com frequência. Estou consciente dos benefícios e estou pensando em começar a incluir atividades na minha rotina.",
+                afirmacao: [
+                    "Reconhecer a importância dos exercícios físicos é o primeiro passo para começar a incorporar atividades na rotina e melhorar a saúde e o bem-estar.",
+                    
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Utilizar informações próprias traz um toque pessoal ao nosso trabalho. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Novas tecnologias trazem novos desafios, por isso após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "É muito importante refletir sobre os trabalhadores do futuro. Por isso a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: [
+                    "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
+                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
+                ],
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: [
+                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
+                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
+                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
+                ],
+            },
+        ]
+    }
+];
