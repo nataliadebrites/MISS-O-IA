0d6843be88aa2bcb6acf21c1efe522801b1d163a
Natália de Brites B. <113113412+nataliadebrites@users.noreply.github.com>
2024-09-05 10:50:05 -0300

    Add files via upload

---
+++ MISS-O-IA-main/js/perguntas.js
@@ -0,0 +1,129 @@
+export const perguntas = [
+    {
+        enunciado: "Você acredita que sua alimentação atual atende aos critérios de uma dieta saudável e equilibrada, ou há áreas que você acha que poderiam ser melhoradas?",
+        alternativas: [
+            {
+                texto: "Sim, possuo uma alimentação saúdavel!",
+                afirmacao: [
+                    "Sua alimentação saudável é a base perfeita para uma vida cheia de energia e bem-estar.",
+                    "Você está investindo no seu futuro com cada escolha alimentar saudável que faz."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Não, minha alimentação não é saúdavel...",
+                afirmacao: [
+                    "Adotar hábitos alimentares mais saudáveis pode transformar sua saúde e bem-estar.",
+                    "Cada passo em direção a uma alimentação equilibrada é um avanço significativo para uma vida mais saudável."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Manter uma alimentação regular é fundamental para a saúde e o bem-estar, tanto mental quanto físico. Estabelecer horários fixos para refeições, como café da manhã, almoço e jantar, pode ser uma excelente estratégia.  Qual dessas duas opçoes seria uma boa escolha para você?",
+        alternativas: [
+            {
+                texto: "Iogurte grego natural com frutas frescas, uma porção de granola e uma colher de chá de sementes de chia.", 
+                afirmacao: [
+                    "Para quem escolheu o iogurte grego com frutas, granola e sementes de chia: você fez uma excelente escolha! Esse café da manhã é nutritivo e fornece uma boa combinação de proteínas, fibras e antioxidantes para começar bem o dia.",
+                    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Donuts e um café com bastante açúcar.",
+                afirmacao: [
+                    "Para quem optou pelos donuts e café com açúcar: essa não foi a melhor escolha. Esse tipo de café da manhã pode levar a picos de açúcar no sangue e falta de energia, e pode não fornecer os nutrientes necessários para um bom início de dia.",
+                    
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Quais são alguns hábitos simples que você pode adotar para tornar sua alimentação mais saudável no dia a dia?",
+        alternativas: [
+            {
+                texto: "Beber água regularmente ao longo do dia.",
+                afirmacao: [
+                    "A hidratação diária promove energia e bem-estar.",
+                    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Beber bebidas açucaradas, como refrigerantes e sucos industrializados, em vez de água.",
+                afirmacao: [
+                    "Aumentar a ingestão de água é essencial para melhorar a saúde e a vitalidade.",
+                    
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Exercícios físicos são essenciais para manter a saúde, fortalecendo o coração, os músculos e melhorando a flexibilidade. Além disso, ajudam a reduzir o estresse e melhorar o humor. Com tantos benefícios, a atividade física é crucial para uma vida saudável. E você, pratica exercícios físicos regularmente?",
+        alternativas: [
+            {
+                texto: "Sim, pratico exercícios físicos regularmente. Sinto que isso realmente melhora minha energia, saúde e bem-estar geral.",
+                afirmacao: [
+                    "Manter uma rotina regular de exercícios físicos contribui significativamente para a saúde e o bem-estar, trazendo energia e melhorando a qualidade de vida.",
+                    
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Não, atualmente não pratico exercícios físicos com frequência. Estou consciente dos benefícios e estou pensando em começar a incluir atividades na minha rotina.",
+                afirmacao: [
+                    "Reconhecer a importância dos exercícios físicos é o primeiro passo para começar a incorporar atividades na rotina e melhorar a saúde e o bem-estar.",
+                    
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Após responder a todas as perguntas, você acha que sua vida é saudável?",
+        alternativas: [
+            {
+                texto: "Sim, tenho minha alimentação saúdavel, e tenho certeza disso!",
+                afirmacao: [
+                    "Ficamos muito felizes com seu desempenho em sua vida saúdavel!",
+                   
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Não tenho certeza se estou tendo minha alimentação saúdavel...",
+                afirmacao: [
+                    "É sempre bom manter o habito de se alimentar bem, e práticar exercícios diariamente...",
+                   
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Agora vamos tirar nossa prova real!",
+        alternativas: [
+            {
+                texto: "Vamos nessa!",
+                afirmacao: [
+                    "Parabéns por investir em uma alimentação equilibrada e manter uma rotina de exercícios! Sua dedicação à saúde é admirável e certamente contribui para seu bem-estar geral e qualidade de vida.",
+                   
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Vamos nessa!",
+                afirmacao: [
+                    "Reconhecer que há áreas para melhorar é o primeiro passo para uma vida mais saudável. Começar a adotar hábitos alimentares mais equilibrados e incluir a prática de exercícios na rotina pode fazer uma grande diferença no seu bem-estar e saúde geral.",
+                    
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    
+];
